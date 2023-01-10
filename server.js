6e21ec6b0d3363fe18eaad6e95c7a8f1e48ed0c0
TacTac <110001003+KJKhalil@users.noreply.github.com>
2023-01-10 17:36:12 -0500

    created server.js and creates a PORT code as well as the dependencies

---
+++ server.js
@@ -0,0 +1,8 @@
+const PORT = process.env.PORT || 3001;
+
+// Dependencies
+
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const data = require('.db/db.json');
