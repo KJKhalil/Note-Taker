00600d87591f61331d98b57e2ffff8bfd8a52083
TacTac <110001003+KJKhalil@users.noreply.github.com>
2023-01-25 21:16:12 -0500

    Added the app.post code

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended : true }));
 
-// Gets the html's and the data(the notes) saved.
+// Gets the html's and the data(the notes) that are saved.
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
@@ -27,4 +27,23 @@ app.get('/notes', (req, res) => {
 });
 app.get('/api/notes', (req, res) => {
     res.json(data);
+});
+
+app.post('/api/notes', (req, res) => {
+    let newNote = req.body;
+    let highestId = 99;
+    for (let i = 0; i < data.length; i++) {
+        let createdNote = data[i];
+        if (createdNote.id > highestId) {            
+            highestId = createdNote.id;
+        }
+    }
+
+    newNote.id = highestId + 1;
+
+    data.push(newNote)
+
+    fs.writeFile (path.join(__dirname, './db/db.json'), JSON.stringify(data));
+
+    res.json(newNote);
 });
